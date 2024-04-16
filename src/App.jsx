71986dbf2a6b5cd7dfae502befee94a5f778b7d6
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-15 20:16:47 -0400

    import section comps to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter } from "react-router-dom";
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      3D Lorraine Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className="relative z-0">
+          {/* display 3D starts here */}
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
