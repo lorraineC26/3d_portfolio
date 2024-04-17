1e0ab683fb9e620589b392d434aeb8bbe80b3eeb
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-17 15:35:45 -0400

    implement scrolling icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+// import { repeat } from "maath/dist/declarations/src/misc";
 
 const Hero = () => {
   return (
@@ -22,15 +23,33 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100 `}>
             Versatile Full-Stack Web Developer merging biotech and medical
-            writing skills. <br className="sm:block hidden"/> 
-            Proficient in end-to-end development, passionate about tech solutions 
-            across diverse fields, including healthcare and beyond.
+            writing skills. <br className="sm:block hidden" />
+            Proficient in end-to-end development, passionate about tech
+            solutions across diverse fields, including healthcare and beyond.
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
 
+      {/* scroll-down icon */}
+      <div className="absolute xs:bottom-13 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0], //only move on the y-axis
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
