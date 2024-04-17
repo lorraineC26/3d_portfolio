f3cfd2a234ec399616f07306e4f09b8e572466a1
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-17 14:44:09 -0400

    adjust light index

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,9 +14,25 @@ const Computers = () => {
   return (
     // use mesh instead of div for 3d models
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={3.8} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      
+      {/* the actual model */}
+      <primitive
+        object={computer.scene}
+        //set location of the model
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
