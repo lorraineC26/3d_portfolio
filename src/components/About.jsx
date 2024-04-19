3d9f15c75ecb9c00808e101a4db4e0f57b4d5802
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-19 13:25:54 -0400

    add overview paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt"; // tilting cards when hovering
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        {/* fadeIn accepts 4 params: direction, type, delay(s), ration of animation */}
+        I'm a dynamic Full-Stack Web Developer with a background in nutrition
+        and biotech. From biotech internship to medical writing role, I've
+        developed strong communication, teamwork, and problem-solving skills. As
+        a Junior Frontend Developer at a start-up, I tackled projects using
+        TypeScript, JavaScript, React, and NextJS. With a knack for synthesizing
+        complex information, I'm poised to craft impactful solutions. Let's
+        collaborate to bring innovative ideas to fruition and make a meaningful
+        impact!
+      </motion.p>
+    </>
+  );
+};
+
+export default About;
