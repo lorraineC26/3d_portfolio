d3cf742be1d8c30b87b09a6c31deb47dd227e5fd
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-20 00:35:24 -0400

    add mock task descrptions for each exp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -38,13 +38,14 @@ const ExperienceCard = ({ experience }) => {
         </p>
       </div>
 
-      {/* For tasks description */}
+      {/* What you did at each company */}
       <ul className="mt-5 list-disc ml-5 space-y-2">
         {experience.points.map((point, index)=>(
           <li
             key={`experience-point-${index}`}
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
           >
-
+            {point}
           </li>
         ))}
 
