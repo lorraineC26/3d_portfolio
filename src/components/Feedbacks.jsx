5cf4d04cb50fcdf21cfe797cde520e427e401771
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-22 23:56:19 -0400

    turn each feedback into a feedback card

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -13,7 +13,16 @@ const Feedbacks = () => {
           <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testmonails.</h2>
         </motion.div>
+      </div>
 
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index)=>(
+          <FeedbackCard 
+            key={testimonial.name} 
+            index={index} 
+            {...testimonial}
+          />
+        ))}
       </div>
 
     </div>
