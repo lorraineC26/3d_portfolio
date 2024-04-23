95292fea5e7018ee9b123aaf5bbb3d005134c616
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-22 23:23:06 -0400

    make github icon appear on each project thumbnail

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -20,11 +20,29 @@ const ProjectCard = ({ index, name, descripition, tags, image, source_code_link 
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
+          {/* project thumbnails */}
           <img 
             src={image}
             alt={name}
             className="w-full h-full object-cover rounded-2xl"
           />
+
+          {/* github redirection icon */}
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            {/* open page in a new link */}
+            <div
+               onClick={()=> window.open(source_code_link, "_blank")}
+               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+
+            </div>
+
+          </div>
 
         </div>
       </Tilt>
