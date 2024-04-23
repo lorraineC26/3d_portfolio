71836e13b7b580b35d3246fc9e758fe2138d66f2
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-22 23:39:42 -0400

    make used tech stacks appeared

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -47,8 +47,17 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
 
       {/* For name & description for each project */}
       <div className="mt-5">
-        <h3 >{name}</h3>
-        <p>{description}</p>
+        <h3 className="text-white font-bold text-[23px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
+
+      {/* For tech stacks used in each project */}
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag)=>(
+          <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
 
       </div>
       </Tilt>
