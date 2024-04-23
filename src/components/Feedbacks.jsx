f332d09063a76dfa8327dc77b8d13d896da8b937
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-22 23:52:36 -0400

    add styled title for feedback sec

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -9,6 +9,10 @@ const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testmonails.</h2>
+        </motion.div>
 
       </div>
 
