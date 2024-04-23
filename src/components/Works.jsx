f0e200fceb3e87498d728f13a1a017d7aa5c6b70
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-22 23:00:28 -0400

    map mock projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -27,6 +27,17 @@ const Works = () => {
           and manage projects effectively.
         </motion.p>
       </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index)=>(
+          <ProjectCard 
+            key={`project-${index}`} 
+            index={index}
+            {...project}
+          />
+        ))}
+
+      </div>
     </>
   );
 }
