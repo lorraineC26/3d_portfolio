02f7afd19c179622b31dfe312db293b5d937e921
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-25 15:19:38 -0400

    impl submit button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -72,6 +72,14 @@ const Contact = () => {
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {/* check the sending state to show the proper button */}
+            {loading ? 'Sending...' : 'Send'}
+          </button>
         </form>
       </motion.div>
     </div>
