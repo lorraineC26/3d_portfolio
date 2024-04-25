1eda2740fffb9f052cbbcd6390d9cb4e31a24609
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-25 15:35:33 -0400

    make earth can be rotated horizontslly

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -10,6 +10,9 @@ const Earth = () => {
   return (
     <primitive
       object={earth.scene}
+      scale={2.5}
+      position-y={0} // ensur we can rotate horizontally
+      rotation-y={0}
     />
   );
 };
