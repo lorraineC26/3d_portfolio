1a3f72575092da6d7fb8de333900100ed9accbae
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-25 15:33:59 -0400

    impl earth canvas and make earth model show

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from '../Loader'
 
 const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf')
+
   return (
-    <div>Earth</div>
-  )
+    <primitive
+      object={earth.scene}
+    />
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      {/* Suspense is going to ensure while our model is loading, we have sth to show */}
+      <Suspense fallback={<CanvasLoader />}>
+        {/* OrbitContrl allow our mouse to move around the model */}
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        {/* the earth model */}
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
 }
 
-export default Earth
+export default EarthCanvas;
