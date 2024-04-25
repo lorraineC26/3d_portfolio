034f96101825d6bc332fc6ff7b1bbc46333d4c95
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-25 15:11:16 -0400

    impl name input field and styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,22 +20,37 @@ const Contact = () => {
 
   const handleChange = (e) => {};
 
-  const handleSubmit =(e) => {};
-
+  const handleSubmit = (e) => {};
 
   return (
-  <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
-    <motion.div
-      variants={slideIn('left', 'tween', 0.2, 1)}
-      // flex-[0.75] means we want to take 3/4 screen
-      className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-    >
-      <p className={styles.sectionSubText}>Get in touch</p>
-      <h3 className={styles.heroHeadText}>COntact.</h3>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        // flex-[0.75] means we want to take 3/4 screen
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.heroHeadText}>Contact.</h3>
 
-    </motion.div>
-
-  </div>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+        </form>
+      </motion.div>
+    </div>
   );
 };
 
