4cc889d6499619668ab587bc5ae8b314d7480741
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-01 23:10:11 -0400

    update self intro

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,11 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-
-const ServiceCard = ({index, title, icon}) => {
-  return(
+const ServiceCard = ({ index, title, icon }) => {
+  return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn("right", "spring", 0.5*index, 0.75)}
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
@@ -20,47 +19,50 @@ const ServiceCard = ({index, title, icon}) => {
           options={{
             max: 45,
             scale: 1,
-            speed: 450
+            speed: 450,
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
-
     </Tilt>
-  )
-}
+  );
+};
 
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>About Me.</h2>
       </motion.div>
 
       {/* fadeIn accepts 4 params: direction, type, delay(sec), ration of animation */}
-      <motion.p 
+      <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a dynamic Full-Stack Web Developer with a background in nutrition
-        and biotech. From biotech internship to medical writing role, I've
-        developed strong communication, teamwork, and problem-solving skills.<br/> As
-        a Junior Frontend Developer at a start-up, I tackled projects using
-        TypeScript, JavaScript, React, and NextJS. With a knack for synthesizing
-        complex information, I'm poised to craft impactful solutions. Let's
-        collaborate to bring innovative ideas to fruition and make a meaningful
-        impact!
+        My coding journey started in my final summer at McGill, studying
+        Nutrition. Serendipitously, I discovered 'Programming in Life Science,'
+        where Python became my tool for patient weight control, DNA analysis,
+        etc. I was hooked from the start, finding the whole experience
+        incredibly fascinating.
+        <br />
+        From biotech to web development at a startup, I've refined
+        communication, teamwork, and problem-solving skills while shaping my
+        technical abilities, including TypeScript, React, NextJS, etc. With a
+        talent for simplifying complex concepts, I'm ready to create meaningful
+        solutions.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
-
       </div>
     </>
   );
