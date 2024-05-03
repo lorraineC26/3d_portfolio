d9d84294644b3a8471cf4067d1be1dc13957b0fc
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-03 01:50:30 -0400

    change logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -31,9 +31,7 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-// import carrent from "./carrent.png";
-// import jobit from "./jobit.png";
-// import tripguide from "./tripguide.png";
+
 import swipeToWork from "./swipeToWork.png";
 import photolabs from "./photolabs.png";
 import jungle from "./jungle.png";
